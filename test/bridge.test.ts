f96137740dcaea7c7bfc253c9b2d7ee281c1292b
Alexey Sadkovich <sadkovich01@mail.ru>
2023-01-22 10:23:36 +0300

    feat: add jettons mint (wip) (#2)
    * fix: update destination chain id size
    
    * feat: add jetton deploy
    
    * fix jetton deployment function
    
    * fix bridge deployment
    
    * add tests for jetton & minor refactor
    
    * clean up unused code

--- test/bridge.test.ts
+++ test/bridge.test.ts
@@ -1,15 +1,25 @@
 import {
   Address,
   beginCell,
+  Cell,
+  Dictionary,
   toNano,
   TransactionComputeVm,
   TransactionDescriptionGeneric,
+  TupleItemCell,
 } from "ton";
 import { ContractExecutor, ContractSystem, Treasure } from "ton-emulator";
-import { createBridge, getBalance } from "./helpers";
+import {
+  buildTokenMetadataCell,
+  deployBridge,
+  deployJettonMinter,
+  getBalance,
+  sha256,
+} from "./helpers";
 import chai, { expect } from "chai";
 import BN from "bn.js";
 import chaiBn from "chai-bn";
+import { inspect } from "util";
 
 chai.use(chaiBn(BN));
 
@@ -23,17 +33,48 @@ describe("Test wrapped swap", () => {
     system = await ContractSystem.create();
     treasure = system.treasure("random-treasure");
     oracleTreasure = system.treasure("oracle-treasure");
-    bridge = await createBridge(system, oracleTreasure);
+    bridge = await deployBridge(system, oracleTreasure);
+  });
+
+  it("checks bridge data", async () => {
+    const bridgeData = await bridge.get("get_bridge_data");
+    expect(bridgeData.success).to.be.true;
+
+    if (bridgeData.success) {
+      expect(bridgeData.stack.readNumber()).to.be.equal(0); // admin address workchain
+      expect(bridgeData.stack.readBigNumber().toString(16)).to.be.equal(
+        oracleTreasure.address.hash.toString("hex")
+      ); // admin address hash
+
+      // Check that oracle was added to the bridge.
+      expect(
+        bridgeData.stack
+          .readCell()
+          .beginParse()
+          .loadDictDirect(
+            Dictionary.Keys.Buffer(32),
+            Dictionary.Values.Dictionary(
+              Dictionary.Keys.Uint(32),
+              Dictionary.Values.BigUint(160)
+            )
+          )
+          .has(oracleTreasure.address.hash)
+      ).to.be.true;
+
+      expect(bridgeData.stack.pop().type).to.be.equal("null"); // empty jettons dict
+      expect(bridgeData.stack.pop().type).to.be.equal("cell"); // jetton minter code
+      expect(bridgeData.stack.pop().type).to.be.equal("cell"); // jetton wallet code
+    }
   });
 
   it("should lock TONs and emit log message", async () => {
     const destinationAddress = 0x142d6db735cdb50bfc6ec65f94830320c6c7a245n;
-    const destinationChainId = 1;
+    const destinationCoinId = 1;
     const value = toNano(2);
 
     const body = beginCell()
       .storeUint(destinationAddress, 160)
-      .storeUint(destinationChainId, 8)
+      .storeUint(destinationCoinId, 32)
       .endCell()
       .beginParse();
 
@@ -54,7 +95,7 @@ describe("Test wrapped swap", () => {
     let cs = resp.get(0)?.body.beginParse()!;
 
     const logDestinationAddress = cs.loadUintBig(160).toString(16);
-    const logDestinationChainId = cs.loadUint(8);
+    const logDestinationCoinId = cs.loadUint(32);
     const logFromAddressHash = cs.loadUintBig(256).toString(16);
     const logFromAddress = Address.parseRaw(
       bridge.address.workChain + ":" + logFromAddressHash
@@ -62,7 +103,7 @@ describe("Test wrapped swap", () => {
     const logMsgValue = cs.loadUintBig(64);
 
     expect(logDestinationAddress).to.be.equal(destinationAddress.toString(16));
-    expect(logDestinationChainId).to.be.equal(destinationChainId);
+    expect(logDestinationCoinId).to.be.equal(destinationCoinId);
     expect(logFromAddress.equals(treasure.address)).to.be.true;
     expect(logMsgValue).to.be.equal(value);
   });
@@ -112,7 +153,7 @@ describe("Test wrapped swap", () => {
   it("should fail to unlock TONs because of non oracle account", async () => {
     const destinationAddress = treasure.address;
     const value = toNano(2);
-    const feeValue = 10000000n;
+    const feeValue = 100000000n;
 
     const body = beginCell()
       .storeAddress(destinationAddress)
@@ -148,5 +189,110 @@ describe("Test wrapped swap", () => {
 
     // 402 exit code - the sender is not an oracle.
     expect(computePhase.exitCode).to.be.equal(402);
+  });
+
+  it("should add new jetton to bridge", async () => {
+    const jettonCoinId = 1729;
+    const metadata = buildTokenMetadataCell({
+      name: "Wrapped TZS",
+      symbol: "bTZS",
+      image: "https://example.com/image.png",
+      description: "some description for the test jetton",
+    });
+    const feeValue = toNano(1);
+
+    const jetton = await deployJettonMinter(
+      system,
+      oracleTreasure,
+      bridge.address,
+      metadata
+    );
+
+    const body = beginCell()
+      .storeUint(jettonCoinId, 32)
+      .storeAddress(jetton.address)
+      .endCell()
+      .beginParse();
+
+    await oracleTreasure.send({
+      sendMode: 0,
+      to: bridge.address,
+      value: feeValue,
+      body: beginCell()
+        .storeUint(3, 32) // op
+        .storeUint(111, 64) // query id
+        .storeSlice(body)
+        .endCell(),
+      bounce: false,
+    });
+
+    let txs = await system.run();
+
+    expect(
+      txs.filter(
+        (tx) =>
+          (tx.description as any).aborted !== undefined &&
+          (tx.description as any).aborted === true
+      ),
+      "Some of transactions aborted"
+    ).to.be.empty;
+
+    const bridgeData = await bridge.get("get_bridge_data");
+    expect(bridgeData.success).to.be.true;
+
+    if (bridgeData.success) {
+      bridgeData.stack.skip(); // skip admin address wc
+      bridgeData.stack.skip(); // skip admin address hash
+      bridgeData.stack.skip(); // skip oracles
+
+      const jettonsDictCell = bridgeData.stack.pop();
+
+      // TODO: c4 is not updated for some reason. A bug in ton-emulator?
+
+      expect(jettonsDictCell.type).to.be.equal("cell");
+
+      const jettonsDict = (jettonsDictCell as TupleItemCell).cell
+        .beginParse()
+        .loadDictDirect(Dictionary.Keys.Uint(32), Dictionary.Values.Address());
+
+      expect(jettonsDict.get(jettonCoinId)).to.be.equal(jetton.address);
+    }
+  });
+
+  it("should mint jetton to destination address", async () => {
+    const destinationAddress = treasure.address;
+    const jettonCoinId = 1;
+    const jettonAmount = 1000n;
+    const feeValue = toNano(1);
+
+    const body = beginCell()
+      .storeAddress(destinationAddress)
+      .storeUint(jettonCoinId, 32)
+      .storeCoins(jettonAmount)
+      .endCell()
+      .beginParse();
+
+    await oracleTreasure.send({
+      sendMode: 0,
+      to: bridge.address,
+      value: feeValue,
+      body: beginCell()
+        .storeUint(21, 32) // op
+        .storeUint(111, 64) // query id
+        .storeSlice(body)
+        .endCell(),
+      bounce: false,
+    });
+
+    let txs = await system.run();
+
+    expect(
+      txs.filter(
+        (tx) =>
+          (tx.description as any).aborted !== undefined &&
+          (tx.description as any).aborted === true
+      ),
+      "Some of transactions aborted"
+    ).to.be.empty;
   });
 });
